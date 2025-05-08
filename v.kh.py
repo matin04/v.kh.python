5765b945e79d73c2130116334113e3ee6c9bd91a
matin04 <fathuddinzodamatin@gmail.com>
2025-05-08 16:39:17 +0300

    6.while

--- v.kh.py
+++ v.kh.py
@@ -1,12 +1,11 @@
-# while 5. /////////////////////
+# while 6. /////////////////////
 
-a=int(input())
 n=int(input())
 s=1
 p=1
 i=1
 while i<=n:
-    p*=a 
-    s+=p
     i+=1
+    p*=2 
+    s+=p 
 print(s) 
