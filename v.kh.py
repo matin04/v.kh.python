c4399937308afe2e698297a11399fa565da85237
matin04 <fathuddinzodamatin@gmail.com>
2025-05-08 17:49:01 +0300

    for 4

--- v.kh.py
+++ v.kh.py
@@ -1,10 +1,12 @@
-# for 3. /////////////////////
+# for 4. /////////////////////
 
 
 n=int(input())
+k=int(input())
 s=1
-for i in range(1,n+1):
-         s*=i
+for i in range(1,k+1):
+         s*=(n-i+1)
+         s//=i
 print(s)
 
 
